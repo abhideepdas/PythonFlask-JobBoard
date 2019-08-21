c63bc22dd5e8020bee742832c5d182772f1f9862
abhideepdas <gorganites@gmail.com>
2019-08-21 10:17:09 +1000

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -12,13 +12,16 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, value=(), commit=False, single=False):
-    conection = open_connection()
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
-        result = connection.commit()
+        results = connection.commit()
     else:
-        result = cursor.fetchone() if single else cursor.fetchall()
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
